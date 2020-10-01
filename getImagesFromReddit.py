24ff808bdcc73fc1c7c09af00c920339bcf8d323
Sam Chapin <schapin@samuelchapin.com>
2020-10-01 15:55:24 -0500

    Added functionality to filter out any non-landscape images.

--- getImagesFromReddit.py
+++ getImagesFromReddit.py
@@ -5,6 +5,7 @@ import random
 import requests
 import re
 import os
+from PIL import Image
 
 REDDIT = praw.Reddit("bot1", user_agent="Post Scraper")
 IMAGE_FORMATS = ["jpeg", "jpg", "png"]
@@ -27,7 +28,7 @@ def get_image():
 
 def download_image(url):
     image = requests.get(url).content
-    image_found = re.search("\w+\.\w+$", url)
+    image_found = re.search(r"\w+\.\w+$", url)
     if image_found:
         image_name = image_found.group()
 
@@ -35,5 +36,17 @@ def download_image(url):
         image_file = open(image_fullpath, "wb")
         image_file.write(image)
         image_file.close()
+
+        if not isLandscapeImage(image_fullpath):
+            os.remove(image_fullpath)
+
+
+def isLandscapeImage(url):
+    image = Image.open(url)
+    if image.width > image.height:
+        return True
+    else:
+        return False
+
 
 get_image()
